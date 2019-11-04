68ab186330e502a5257a00f22efd8048002ef499
Francisco Gutierrez <fran.gu1498@gmail.com>
2019-11-03 21:58:18 -0300

    tablero
    etapa inicial del tablero

---
+++ src/Tablero.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Tablero {
+    private ArrayList<Unidades> ladoAzul = new ArrayList<>;
+    private ArrayList<Unidades> ladoRojo = new ArrayList<>;
+    private Dictionary casilleros = new Hashtable();
+    private Dictionary posicionDeUnidad = new Hashtable()
+
+    public void moverUnidad(Unidades unidad, String casillero){
+        
+    }
+}
