c26a07e413d6b23587363a94acc0150600043a0b
Francisco Gutierrez <fran.gu1498@gmail.com>
2019-11-03 23:33:22 -0300

    tablero moverUnidad

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,12 +1,20 @@
 import java.util.*;
 
 public class Tablero {
-    private ArrayList<Unidades> ladoAzul = new ArrayList<>;
-    private ArrayList<Unidades> ladoRojo = new ArrayList<>;
     private Dictionary casilleros = new Hashtable();
     private Dictionary posicionDeUnidad = new Hashtable()
 
     public void moverUnidad(Unidades unidad, String casillero){
-        
+        Casillero celda = this.casilleros.get(casillero);
+        // si la unidad ya está en el tablero la posiciona en el nuevo casillero
+        if(posicionDeUnidad.containsKey(unidad)){
+            Casillero posicion = posicionDeUnidad.get(unidad);
+            posicion.mover_unidad_a(celda);
+        }
+        //else, la posiciona directamente en el casillero
+        else {
+            celda.recibir_unidad(unidad);
+            posicionDeUnidad.put(unidad, celda);
+        }
     }
 }
