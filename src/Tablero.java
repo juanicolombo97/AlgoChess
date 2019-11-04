8286a91927ef2c700d8ed28f08763c9bd3fc22a8
Francisco Gutierrez <fran.gu1498@gmail.com>
2019-11-04 00:17:12 -0300

    tablero actualizo constructor

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
 public class Tablero {
-    private Dictionary casilleros = new Hashtable();
-    private Dictionary posicionDeUnidad = new Hashtable()
+    private Dictionary casilleros;
+    private Dictionary posicionDeUnidad;
+
+    public Tablero(){
+        this.casilleros = new Hashtable();
+        this.posicionDeUnidad = new Hashtable();
+        for(int i = 1; i < 21; i++){
+            for(int j = 1; j < 21; j++){
+                Casillero casillero = new Casillero();
+                String numi = Integer.toString(i);
+                String numj = Integer.toString(j);
+                String numCasillero = numi + " " + numj;
+                this.casilleros.put(numCasillero, casillero);
+            }
+        }
+    }
 
     public void moverUnidad(Unidades unidad, String casillero){
         Casillero celda = this.casilleros.get(casillero);
