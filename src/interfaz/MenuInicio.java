2c14e56e022a8006c2e983bdc7f46e522953caf3
Juani Colombo <juanicolombo@icloud.com>
2019-11-05 16:48:56 -0300

    boton modif

--- src/interfaz/MenuInicio.java
+++ src/interfaz/MenuInicio.java
@@ -1,37 +1,43 @@
+package interfaz;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class MenuInicio extends Application {
 
     Button boton;
+    Button boton2;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        //Configuro el nombre de la app y los componentes principales
+        //Configuro el nombre del juego y hago la ventana.
         primaryStage.setTitle("AlgoChess");
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 300,250);
+        Pane layout = new Pane();
+        Scene scene = new Scene(layout, 800,600);
 
-        //Configuro boton.
-        boton = new Button("Iniciar Juego");
-        // Creo imagen y la establesco para que se mueva con la app.
+        //Creo botones.
+        boton = new Button("Play");
+        boton.setStyle("-fx-background-image: url('/imagenes/botonPlay.png')");
+        boton2 = new Button("Salir");
+
+        // Creo imagen y la establesco para que se mueva con la ventana.
         final ImageView imagen = new ImageView("imagenes/fondomenu.jpg");
         imagen.fitHeightProperty().bind(scene.heightProperty());
         imagen.fitWidthProperty().bind(scene.widthProperty());
 
-        //Agrego las cosas a la primaryStage.
+        //Agrego las imagen y botones a la primaryStage.
         layout.getChildren().add(imagen);
         layout.getChildren().add(boton);
+        layout.getChildren().add(boton2);
+
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.show();
