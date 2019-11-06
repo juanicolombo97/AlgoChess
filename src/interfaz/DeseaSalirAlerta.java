06dea7090bb8ee7288d45bb9cb73364a3ea75870
Juani Colombo <juanicolombo@icloud.com>
2019-11-06 20:10:13 -0300

    interfaz comienzo

---
+++ src/interfaz/DeseaSalirAlerta.java
@@ -0,0 +1,50 @@
+package interfaz;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class DeseaSalirAlerta {
+
+    static boolean respuesta;
+
+    public static boolean display(String titulo, String mensaje) {
+        Stage ventana = new Stage();
+
+        ventana.initModality(Modality.APPLICATION_MODAL);
+        ventana.setTitle(titulo);
+        ventana.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(mensaje);
+        Button botonSi = new Button("SI");
+        Button botonNo = new Button("No");
+
+        botonSi.setOnAction(e -> {
+            respuesta = true;
+            ventana.close();
+                }
+        );
+        botonNo.setOnAction(e -> {
+                    respuesta = false;
+                    ventana.close();
+                }
+                );
+
+        VBox layout = new VBox(10);
+        Scene scene = new Scene(layout);
+
+        layout.getChildren().addAll(label,botonSi,botonNo);
+        layout.setAlignment(Pos.CENTER);
+
+        ventana.setScene(scene);
+        ventana.showAndWait();
+
+        return respuesta;
+    }
+}
