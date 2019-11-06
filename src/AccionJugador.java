94bb2c228d0506b9b26bce053b623ba1e6666559
Juani Colombo <juanicolombo@icloud.com>
2019-11-06 20:03:27 -0300

    Jugador y Unidades andando

---
+++ src/AccionJugador.java
@@ -0,0 +1,2 @@
+public class IniciarAtaque {
+}
--- src/UnidadNueva.java
+++ src/UnidadNueva.java
@@ -1,10 +1,2 @@
-import excepciones.UnidadInvalidaException;
-
 public class UnidadNueva {
-
-    //Clase que recibe el nombre de la unidad a crear, la crea y se la devuelve al jugador.
-    public Unidades crearUnidad(String unidadACrear) throws UnidadInvalidaException {
-        UnidadesFabrica fabrica = new UnidadesFabrica();
-        return fabrica.crearUnidad(unidadACrear);
-    }
 }
