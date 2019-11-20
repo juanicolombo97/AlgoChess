4de6082a2d9abed78c63fb69facb635ab25ff5fb
salvanescki <nsalvanescki@fi.uba.ar>
2019-11-20 17:19:32 -0300

    WIP Pruebas Casillero

--- src/test/java/fiuba/algo3/algochess/testclases/CasilleroTest.java
+++ src/test/java/fiuba/algo3/algochess/testclases/CasilleroTest.java
@@ -1,6 +1,5 @@
 package fiuba.algo3.algochess.testclases;
 
-import fiuba.algo3.algochess.excepciones.CasilleroOcupadoException;
 import fiuba.algo3.algochess.juego.Casillero;
 import fiuba.algo3.algochess.unidades.Soldado;
 import fiuba.algo3.algochess.unidades.UnidadNula;
@@ -20,17 +19,6 @@ public class CasilleroTest {
         Soldado soldado = new Soldado(1, 2);
         casillero.guardarUnidad(soldado);
         Assert.assertEquals(soldado, casillero.getUnidad());
-    }
-
-    @Test
-    public void modificarUnidadLanzaExcepcionAlEstarVacio(){
-        Casillero casillero = new Casillero();
-        Soldado soldado = new Soldado(3, 4);
-        try{
-            casillero.modificarUnidad(soldado);
-        } catch (CasilleroOcupadoException e){
-
-        }
     }
 
     @Test
