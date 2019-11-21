a78c951ff553685dece3520f108b893350c45502
salvanescki <nsalvanescki@fi.uba.ar>
2019-11-20 22:37:21 -0300

    WIP Pasan las pruebas pero falta implementar batallon en moverUnidad de Jugador

--- src/main/java/fiuba/algo3/algochess/juego/Tablero.java
+++ src/main/java/fiuba/algo3/algochess/juego/Tablero.java
@@ -38,8 +38,7 @@ public class Tablero {
         Unidad unidadCreada = jugador.crearUnidad(posicionX,posicionY,casillero,nombreUnidad);
         casillero.modificarUnidad(unidadCreada);
     }
-
-    public void moverUnidad(int posicionInicialX, int posicionInicialY, int posicionFinalX, int posicionFinalY,Jugador jugador) throws UnidadNulaException, UnidadInvalidaException, MovimientoInvalidoException, CasilleroOcupadoException {
+    /*public void moverUnidad(int posicionInicialX, int posicionInicialY, int posicionFinalX, int posicionFinalY,Jugador jugador) throws UnidadNulaException, UnidadInvalidaException, MovimientoInvalidoException, CasilleroOcupadoException {
         unidadesBatallon =batallon.moverBatallon(arrayCasillero,posicionInicialX,posicionInicialY);
         int distanciaX = posicionFinalX - posicionInicialX;
         int distanciaY = posicionFinalY - posicionInicialY;
@@ -58,6 +57,18 @@ public class Tablero {
         }
 
 
+    }
+*/
+    public void moverUnidad(int posicionInicialX, int posicionInicialY, int posicionFinalX, int posicionFinalY,Jugador jugador) throws UnidadNulaException, fiuba.algo3.algochess.excepciones.UnidadInvalidaException, MovimientoInvalidoException, CasilleroOcupadoException {
+
+        int distanciaX = posicionFinalX - posicionInicialX;
+        int distanciaY = posicionFinalY - posicionInicialY;
+        Unidad unidadAMover = arrayCasillero[posicionInicialX][posicionInicialY].getUnidad();
+
+        arrayCasillero[posicionInicialX][posicionInicialY].eliminarUnidad();
+        arrayCasillero[posicionFinalX][posicionFinalY].modificarUnidad(unidadAMover);
+
+        jugador.moverUnidad(unidadAMover, distanciaX, distanciaY);
     }
 
     public void atacar(int posicionAtacanteX, int posicionAtacanteY, int posicionAtacadoX, int posicionAtacadoY, Jugador jugador) throws NoPuedeAtacarException, UnidadNulaException, CurarException, UnidadInvalidaException {
