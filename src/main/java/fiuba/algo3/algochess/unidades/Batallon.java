98b09049ce3501ad07abcc3f8723df4a8a7312dc
salvanescki <nsalvanescki@fi.uba.ar>
2019-11-29 06:05:42 -0300

    Cambio menor

--- src/main/java/fiuba/algo3/algochess/unidades/Batallon.java
+++ src/main/java/fiuba/algo3/algochess/unidades/Batallon.java
@@ -1,7 +1,6 @@
 package fiuba.algo3.algochess.unidades;
 
 import fiuba.algo3.algochess.excepciones.CasilleroVacioExcepcion;
-import fiuba.algo3.algochess.juego.Casillero;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,8 +21,8 @@ public class Batallon {
 
     public ArrayList listaSoldados(ArrayList listaUnidades){
         ArrayList<Soldado> listaSoldados = new ArrayList<>();
-        for (int x = 0; x < listaUnidades.size(); x++){
-            listaSoldados.add((Soldado) listaUnidades.get(x));
+        for (Object unidadActual : listaUnidades) {
+            listaSoldados.add((Soldado) unidadActual);
         }
         return listaSoldados;
     }
