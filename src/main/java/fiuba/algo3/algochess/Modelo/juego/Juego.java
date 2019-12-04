7014637f73fc9f9694840855fa59b1869e0dea72
salvanescki <nsalvanescki@fi.uba.ar>
2019-12-04 19:41:56 -0300

    Turnos añadidos

--- src/main/java/fiuba/algo3/algochess/Modelo/juego/Juego.java
+++ src/main/java/fiuba/algo3/algochess/Modelo/juego/Juego.java
@@ -5,6 +5,7 @@ public class Juego {
     private Jugador jugadorAliado;
     private Jugador jugadorEnemigo;
     private Jugador jugadorActual;
+    private Jugador jugadorSiguiente;
     private Tablero tablero;
 
     public Juego(Jugador jugador, Jugador jugador2){
@@ -15,8 +16,36 @@ public class Juego {
     public Tablero comenzarJuego(){
         Tablero tablero = new Tablero(jugadorAliado,jugadorEnemigo);
         this.tablero = tablero;
+        this.jugadorActual = jugadorAliado;
+        this.jugadorSiguiente = jugadorEnemigo;
         return tablero;
     }
 
+    public void cambiarTurno(){
+        if (jugadorSiguiente.puedeSeguirJugando()){
+            Jugador auxiliar = jugadorActual;
+            this.jugadorActual = jugadorSiguiente;
+            this.jugadorSiguiente = auxiliar;
+        } else{
+            finalizarJuego();
+        }
+    }
 
+    public void realizarAtaque(Posicion posicionAtacante, Posicion posicionAtacado, Jugador jugador){
+        if (jugadorActual == jugador){
+            tablero.atacar(posicionAtacante, posicionAtacado, jugador);
+        }
+        this.cambiarTurno();
+    }
+
+    public void realizarMovimiento(Posicion posicionInicial, Posicion posicionFinal, Jugador jugador){
+        if (jugadorActual == jugador){
+            tablero.moverUnidad(posicionInicial, posicionFinal, jugador);
+        }
+        this.cambiarTurno();
+    }
+
+    public void finalizarJuego(){
+        // Algo como "Gano el jugador: str(jugadorActual)"
+    }
 }
